c945b2ba88999c4f4597f1b28adbc74579830342
codecShivam <codecshivam@gmail.com>
2024-03-29 23:40:03 +0530

    fix deployement

--- apps/web/app/marketplace/components/Projects.tsx
+++ apps/web/app/marketplace/components/Projects.tsx
@@ -10,22 +10,21 @@ interface Project {
   description: string;
 }
 
-export default function Projects(): JSX.Element {
-  // Split the projectsData into chunks of two
-  const chunks: Project[][] = projectsData.reduce((resultArray: Project[][], item: Project, index: number) => {
-    const chunkIndex: number = Math.floor(index / 2);
+// Function to split an array into chunks
+function splitIntoChunks<T>(array: T[], chunkSize: number): T[][] {
+    const chunks: T[][] = [];
 
-    // Ensure resultArray[chunkIndex] is not undefined
-    if (resultArray[chunkIndex] === undefined) {
-      // Use type assertion to assure TypeScript that resultArray[chunkIndex] is an array
-      resultArray[chunkIndex] = [] as Project[];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        const chunk: T[] = array.slice(i, i + chunkSize);
+        chunks.push(chunk);
     }
 
-    // Push the item to resultArray[chunkIndex]
-    resultArray[chunkIndex].push(item)
+    return chunks;
+}
 
-    return resultArray;
-  }, []);
+export default function Projects(): JSX.Element {
+  // Split the projectsData into chunks of two
+  const chunks: Project[][] = splitIntoChunks(projectsData, 2);
 
   return (
     <div className="flex flex-col xl:gap-16 place-items-center space-y-[4.688rem] xl:px-24 sm:px-12 px-6">
@@ -33,7 +32,7 @@ export default function Projects(): JSX.Element {
         <div key={rowIndex} className="flex sm:gap-12 gap-[4.688rem] justify-between w-full sm:flex-nowrap flex-wrap">
           {chunk.map((project: Project, colIndex: number) => (
             <Card
-              key={rowIndex * 2 + colIndex} // Ensure each Card has a unique key
+              key={`${rowIndex}_${colIndex}`} // Ensure each Card has a unique key
               imageSrc={project.imageSrc}
               iconSrc={project.iconSrc}
               title={project.title}
